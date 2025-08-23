25b73f25c3fcfc42a0a66ebfe837d4a189b787f8
Vignesh Mariappan <vignesh.mariappan@arrise.com>
2025-08-23 22:21:41 +0530

    _redirects and firebase config settings

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,5 +1,3 @@
-
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
@@ -10,13 +8,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyABmPRybsLJU0xNO_JDuNG7U1hA_VVSWJU",
-  authDomain: "familyvault-a0403.firebaseapp.com",
-  projectId: "familyvault-a0403",
-  storageBucket: "familyvault-a0403.firebasestorage.app",
-  // storageBucket: "familyvault-a0403.appspot.com",
-  messagingSenderId: "587021240696",
-  appId: "1:587021240696:web:d651bdc919c408f9e3608d"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
